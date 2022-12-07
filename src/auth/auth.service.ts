7a5dd05b8fa9acf8ac87b5ac288427654b96d94d
JonHdez <Jonathan.DHernandezR@gmail.com>
2022-12-06 20:30:12 -0600

    Probando auth login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cliente, ClienteDocument } from 'src/schemas/cliente.schema';
@@ -7,7 +7,31 @@ import { Cliente, ClienteDocument } from 'src/schemas/cliente.schema';
 export class AuthService {
     constructor(@InjectModel(Cliente.name) private clienteModel: Model<ClienteDocument>) {}
 
+    checkPassword(passCliente: string, passBD: string): boolean {
+        let chek:boolean;
+        if (passCliente === passBD){
+            chek = true
+        }else{
+            chek = false
+        }
+        return chek
+    }
+    
     async register(cliente:Cliente) {
         return this.clienteModel.create(cliente)
     }
+
+    async login(cliente:Cliente) {
+        const { correo, pasword } = cliente
+        const findUser = await this.clienteModel.findOne({ correo });
+        if (!findUser) throw new HttpException('USER_NOT_FOUND', 404);
+        
+        const checkPass = this.checkPassword(pasword, findUser.pasword);
+        if (!checkPass) throw new HttpException('PASSWORD_INCORRECT', 403);
+
+        const data = findUser;
+        return data;
+    }
+
+
 }
